64e1f7bd6b442ae56d7e8e9d9a95c240124dfc50
avara1986 <a.vara.1986@gmail.com>
2015-06-03 13:36:37 +0200

    Created Front-END

--- web/js/functions.js
+++ web/js/functions.js
@@ -2,8 +2,9 @@ $('#website').keyup(function(e) {
   if (e.keyCode == 13) { sendWebsite();return false; }   // esc
 });
 function sendWebsite(){
-	$('#loading').css('display','block');
-    $('.alert-danger').css('display','none');
+    $('.error').css('display','none');
+    $('.info').css('display','none');
+    $('#loading_website').css('display','block');
     $.ajax({
         method: "POST",
         dataType: "json",
@@ -12,21 +13,143 @@ function sendWebsite(){
         },
         url: "/api/website/",
         success: function(data) {
-        	$('#loading').css('display','none');
-            $('#website_info').html('ID '+data.id+'. TOKEN' + data.token + '');
+        	$('.loading').css('display','none');
+        	$('#website_id').val(data.id);
+        	$('#token').val(data.token);
+        	$('#website_info').css('display','block');
+        	$('#website_info_msg').addClass('alert');
+        	$('#website_info_msg').addClass('alert-success');
+            $('#website_info_msg').html('La web <b>'+$('#website').val()+'</b> fue analizada correctamente');
+            getWebsiteCSS();
         },
         statusCode: {
             404: function() {
-                $('#loading').css('display','none');
+                $('.loading').css('display','none');
                 $('#website_error').css('display','block');
                 
             },
             500: function() {
-                $('#loading').css('display','none');
+                $('.loading').css('display','none');
                 $('#website_error').css('display','block');
                 
             }
           }
       });
     
+}
+function getWebsiteCSS(){
+    $('.error').css('display','none');
+    $('#loading_css').css('display','block');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/api/website/"+$('#website_id').val()+"/"+$('#token').val(),
+        success: function(data) {
+            $('.loading').css('display','none');
+            $('#css_info').css('display','block');
+            result = "<ul>";
+            $.each(data.css, function(i, item) {
+                result += ('<li id="css_'+data.css[i].id+'">'+data.css[i].url+'<a href="#" onclick="sendCss(\''+data.css[i].id+'\');"><i class="glyphicon glyphicon-search"></i></a>');
+            });
+            result += "<ul>";
+            $('#css_info_msg').html(result);
+            },
+        statusCode: {
+            404: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            },
+            405: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            },
+            500: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            }
+          }
+      });
+    
+}
+function sendCss(id_css){
+    $('.error').css('display','none');
+    $('#css_info_'+id_css).remove();
+    $('#css_'+id_css).append(genLoading(id_css));
+    $('#loading_css_'+id_css).css('display','block');
+    $.ajax({
+        method: "POST",
+        dataType: "html",
+        data: {
+            id: id_css,
+            token: $('#token').val(),
+        },
+        url: "/api/css/",
+        success: function(data) {
+            //alert('llega');
+            $('#loading_css_'+id_css).remove();
+            
+            $('.loading').css('display','none');
+            $('#css_'+id_css).append('<div id="css_info_'+id_css+'">\
+                    <h3>El CSS fue generado correctamente</h3>\
+                    </div>');
+            getCss(id_css);
+        },
+        statusCode: {
+            404: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            },
+            500: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            }
+          }
+      });
+    
+}
+function getCss(id_css){
+    $('.error').css('display','none');
+    $('.loading').css('display','none');
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "/api/css/"+id_css+"/"+$('#token').val(),
+        success: function(data) {
+            $('#css_info_'+id_css).html("");
+            $('#css_info_'+id_css).append('<h4>Original comprimido</h4>\
+                    <textarea onclick="this.focus();this.select()">'+data.original_compressed+'</textarea>\
+                    <h4>Limpio</h4>\
+                    <textarea onclick="this.focus();this.select()">'+data.beauty+'</textarea>\
+                    <h4>Limpio comprimido</h4>\
+                    <textarea onclick="this.focus();this.select()">'+data.beauty_compressed+'</textarea>\
+                    </div>');
+        },
+        statusCode: {
+            404: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            },
+            500: function() {
+                $('.loading').css('display','none');
+                $('#css_error').css('display','block');
+                
+            }
+          }
+      });
+    
+}
+function genLoading(id){
+    return '<div class="row loading" id="loading_css_'+id+'">\
+        <div  class="col-lg-12 col-md-12 col-sm-12 col-xs-12">\
+        <div class="alert alert-info" role="alert">\
+            <i class="fa fa-spinner fa-spin"></i> Analizando CSS...\
+        </div>\
+    </div>\
+    </div>';
 }
