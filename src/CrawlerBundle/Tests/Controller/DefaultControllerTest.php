a27284cd62e39d861ffbae7e92ff0899f58577f4
avara1986 <a.vara.1986@gmail.com>
2015-06-17 17:52:43 +0200

    added Functional Tests

--- src/CrawlerBundle/Tests/Controller/DefaultControllerTest.php
+++ src/CrawlerBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,61 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $result;
+
+    private $result_css;
+
+    public function testSendWebsite()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('POST', '/api/website/', array('website' => 'http://www.we-ma.com'));
+        // Assert that the response status code is 2xx
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        // Assert a specific 200 status code
+        $this->assertEquals(
+            200, // or Symfony\Component\HttpFoundation\Response::HTTP_OK
+            $client->getResponse()->getStatusCode()
+        );
+        $this->result = json_decode($client->getResponse()->getContent(), true);
 
-        //$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertGreaterThan(0, $this->result['id']);
+
+
+        $crawler = $client->request('GET', '/api/website/'.$this->result['id'].'/'.$this->result['token']);
+
+        $this->result_css = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertGreaterThan(0, count($this->result_css));
+
+    }
+    public function testgetWebsiteCSS()
+    {
+        $client = static::createClient();
+
+    }
+    public function testErrorWebsite()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/api/website/', array('website' => 'http://atenea.solutions'));
+
+        $this->assertEquals(
+                405,
+                $client->getResponse()->getStatusCode()
+        );
+        $crawler = $client->request('POST', '/api/website/', array('website' => ''));
+
+        $this->assertEquals(
+                404,
+                $client->getResponse()->getStatusCode()
+        );
+
+        $crawler = $client->request('POST', '/api/website/', array('website' => 'http://url-que-no-existe-blablabla-no-no-no.com'));
+
+        $this->assertEquals(
+                500,
+                $client->getResponse()->getStatusCode()
+        );
     }
 }
